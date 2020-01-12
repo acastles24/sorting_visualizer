31e09f7c1ded9df3bb34561f6e950c23933cabb5
Adam <55118346+Jman24@users.noreply.github.com>
2020-01-12 18:45:15 -0500

    Set up packages for testing

---
+++ src/test/algorithmsTest.test.js
@@ -0,0 +1,11 @@
+import {mergeSort} from '../SortingAlgorithms/mergeSort'
+// import {quickSort} from '../SortingAlgorithms/quickSort.js'
+// import {bubbleSort} from '../SortingAlgorithms/bubbleSort.js'
+// import {insertionSort} from '../SortingAlgorithms/insertionSort.js'
+// import {highlightBar, resetBarColor, animationScaledTimeout} from '../SortingAlgorithms/utilities'
+
+it("adds correctly", () => {
+    expect(1+1).toEqual(2);
+ });
+
+// jest.mock('highlightBar', () => jest.fn())
